89153bf1ce32354abc80b7db39e6eb3a3f2a2caf
Liuyikai152 <liuyknet@163.com>
2018-12-06 19:58:05 +0800

    刘奕凯
    D

---
+++ SunFlower/SunFlower.Services/FoodService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SunFlower.MODEL;
+using SunFlower.IServices;
+namespace SunFlower.Services
+{
+    /// <summary>
+    /// 菜品实现类
+    /// </summary>
+    public class FoodService : IFood
+    {
+        public int AddFood(Food food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteFood(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateFood(Food food)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
