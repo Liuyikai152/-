820f2eada79e83a5596ef268dd09a32fb6f98dc3
MoLingMoMing <45552951+MoLingMoMing@users.noreply.github.com>
2018-12-07 20:22:11 +0800

    kyy
    用户日志

---
+++ SunFlower/SunFlower.Api/Controllers/UserLogsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using SunFlower.MODEL;
+using SunFlower.Services;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess;
+
+namespace SunFlower.Api.Controllers
+{
+    public class UserLogsController : ApiController
+    {
+        UserLogsServce servce = new UserLogsServce();
+
+        /// <summary>
+        /// 添加日志
+        /// </summary>
+        /// <param name="userLogs"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public int AddLogs(UserLogs userLogs)
+        {
+
+            int add = servce.AddLogs(userLogs);
+                return add;
+        
+        }
+
+        /// <summary>
+        /// 查看日志
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<UserLogs> GetUserLogs()
+        {
+            var userLogsList = servce.GetUserLogs();
+            return userLogsList;
+        }
+
+    }
+}
