b4f15aaf09a2ead75c7d3b4a0aa0c6f6c71b38f2
MoLingMoMing <45552951+MoLingMoMing@users.noreply.github.com>
2018-12-07 20:59:23 +0800

    kyy
    购物车

---
+++ SunFlower/SunFlower.Api/Controllers/TrolleyController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+using SunFlower.MODEL;
+using SunFlower.Services;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess;
+
+namespace SunFlower.Api.Controllers
+{
+    public class TrolleyController : ApiController
+    {
+        TrolleyService service = new TrolleyService();
+
+
+        /// <summary>
+        /// 添加购物车
+        /// </summary>
+        /// <param name="trolley"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public int AddTrolley(Trolley trolley)
+        {
+
+
+            int add = service.AddTrolley(trolley);
+                return add;
+            
+        }
+
+        /// <summary>
+        /// 删除购物车
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public int DeleteTrolley(int ID)
+        {
+
+            int delete = service.DeleteTrolley(ID);
+                return delete;
+            
+        }
+
+        /// <summary>
+        /// 获取购物车
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<Trolley> GetTrolleys()
+        {
+
+            var trolleyList = service.GetTrolleys();
+                return trolleyList;
+            
+        }
+
+        /// <summary>
+        /// 修改购物车
+        /// </summary>
+        /// <param name="trolley"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public int UpdateTrolley(Trolley trolley)
+        {
+
+            int update = service.UpdateTrolley(trolley);
+            return update;
+            
+        }
+    }
+}
---
+++ SunFlower/SunFlower.Services/TrolleyService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SunFlower.Common;
+using SunFlower.IServices;
+using SunFlower.MODEL;
+using Oracle.DataAccess.Client;
+using Dapper;
+
+namespace SunFlower.Services
+{
+
+    /// <summary>
+    ///购物车实现 
+    /// </summary>
+    public class TrolleyService : ITrolley
+    {
+        
+        /// <summary>
+        /// 添加购物车
+        /// </summary>
+        /// <param name="trolley"></param>
+        /// <returns></returns>
+        public int AddTrolley(Trolley trolley)
+        {
+            using (OracleConnection conn = DapperHelper.GetConnString()) {
+
+                conn.Open();
+                string sql = @"insert into trolley(trolleynumber,userid,storenumber,foodnumber,createtime,money,userphone,addressid) values(:trolleynumber,:userid,:storenumber,:foodnumber,:createtime,:money,:userphone,:addressid)";
+                int add = conn.Execute(sql, trolley);
+                return add;
+            }
+        }
+
+        /// <summary>
+        /// 删除购物车
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public int DeleteTrolley(int ID)
+        {
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+
+                conn.Open();
+                string sql = @"delete from  trolley where id=:id)";
+                int delete = conn.Execute(sql, new { id=ID});
+                return delete;
+            }
+        }
+
+        /// <summary>
+        /// 获取购物车
+        /// </summary>
+        /// <returns></returns>
+        public List<Trolley> GetTrolleys()
+        {
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+
+              
+                string sql = @"select a.trolleynumber,b.username,c.storename,a.money,a.userphone,d.address from trolley a join users b on a.userid=b.id join store c on a.storenumber=c.storenumber join user_adders d on a.addressid=d.id";
+                var trolleyList = conn.Query<Trolley>(sql,null);
+                return trolleyList.ToList() ;
+            }
+        }
+
+        /// <summary>
+        /// 修改购物车
+        /// </summary>
+        /// <param name="trolley"></param>
+        /// <returns></returns>
+        public int UpdateTrolley(Trolley trolley)
+        {
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+
+                conn.Open();
+                string sql = @"update trolley set trolleynumber=:trolleynumber,userid=:userid,storenumber=:storenumber,foodnumber=:foodnumber,createtime=:createtime,money=:money,userphone=:userphone,addressid=:addressid where id=:id";
+                int update = conn.Execute(sql, trolley);
+                return update;
+            }
+        }
+    }
+}
