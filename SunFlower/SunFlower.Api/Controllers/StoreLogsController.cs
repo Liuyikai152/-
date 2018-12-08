65ac65062d558cd3f66617471faaf6046e6fa116
liuyikai152 <liuyknet.163.com>
2018-12-08 11:17:54 +0800

    ytq
    StoreLogs

---
+++ SunFlower/SunFlower.Api/Controllers/StoreLogsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SunFlower.Api.Controllers
+{
+    using SunFlower.MODEL;
+    using SunFlower.Services;
+    using Oracle.DataAccess.Client;
+    using Oracle.DataAccess;
+
+    public class StoreLogsController : ApiController
+    {
+        StoreLogsService servce = new StoreLogsService();
+
+        /// <summary>
+        /// 添加日志
+        /// </summary>
+        /// <param name="storeLogs"></param>
+        /// <returns></returns>
+        public int AddStoreLogs(StoreLogs storeLogs)
+        {
+            int result = servce.AddStoreLogs(storeLogs);
+            return result;
+        }
+
+        /// <summary>
+        /// 显示日志
+        /// </summary>
+        /// <returns></returns>
+        public List<StoreLogs> GetStoreLogs()
+        {
+            var stireLogsList = servce.GetStoreLogs();
+            return stireLogsList;
+        }
+    }
+}
---
+++ SunFlower/SunFlower.Services/StoreLogsService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SunFlower.IServices;
+using SunFlower.MODEL;
+using Oracle.DataAccess.Client;
+using Dapper;
+using SunFlower.Common;
+
+namespace SunFlower.Services
+{
+    /// <summary>
+    /// 日志实现类
+    /// </summary>
+    public class StoreLogsService : IStoreLogs
+    {
+        /// <summary>
+        /// 添加日志
+        /// </summary>
+        /// <param name="storeLogs"></param>
+        /// <returns></returns>
+        public int AddStoreLogs(StoreLogs storeLogs)
+        {
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"insert into StoreLogs(Userid,Storeid,Createtime) values(:Userid,:Storeid,:Createtime)";
+                int result = conn.Execute(sql, storeLogs);
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 显示日志
+        /// </summary>
+        /// <returns></returns>
+        public List<StoreLogs> GetStoreLogs()
+        {
+            using (OracleConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = @"select * from  UserLogs";
+                var storeLogsList = conn.Query<StoreLogs>(sql, null);
+                return storeLogsList.ToList();
+            }
+        }
+    }
+}
