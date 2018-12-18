1dde315ddda5e081987cd50beb20ba2af257efde
Liuyikai152 <liuyknet@163.com>
2018-12-18 09:22:17 +0800

    lyk

---
+++ SunFlower/SunFlower.MODEL/AppNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunFlower.MODEL
+{
+    /// <summary>
+    /// 审批节点表
+    /// </summary>
+    public class AppNode
+    {
+        /// <summary>
+        /// 节点ID
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 审批ID
+        /// </summary>
+        public int AuditID { get; set; }
+
+        /// <summary>
+        /// 节点名称
+        /// </summary>
+        public string NodeName { get; set; }
+
+        /// <summary>
+        /// 节点描述
+        /// </summary>
+        public string NodeContext { get; set; }
+
+    }
+}
---
+++ SunFlower/SunFlower.MODEL/Audits.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunFlower.MODEL
+{
+    /// <summary>
+    /// 审批表
+    /// </summary>
+    public class Audits
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 业务ID
+        /// </summary>
+        public int BusinessID { get; set; }
+
+        /// <summary>
+        /// 审批名称
+        /// </summary>
+        public string AuditName { get; set; }
+        
+        /// <summary>
+        /// 审批描述
+        /// </summary>
+        public string AuditContext { get; set; }
+    }
+}
