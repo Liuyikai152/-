3bd62448bccc09045021ee5fb6b4c3a5fba54ccb
liuyikai152 <liuyknet.163.com>
2018-12-20 18:59:54 +0800

    ytq

---
+++ SunFlower/SunFlower.Api/Controllers/ApprovalActivityController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Data;
+using SunFlower.MODEL;
+using SunFlower.Services;
+using Unity.Attributes;
+using SunFlower.IServices;
+
+namespace SunFlower.Api.Controllers
+{
+    [RoutePrefix("ApprovalActivity")]
+    public class ApprovalActivityController : ApiController
+    {
+        [Dependency]
+        public IApprovalActivity ApprovalActivity { get; set; }
+
+        /// <summary>
+        /// 添加审核活动
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("AddApprovalActivity")]
+        public int AddApprovalActivity()
+        {
+            var result = ApprovalActivity.AddApprovalActivity();
+            return result;
+        }
+
+        /// <summary>
+        /// 显示待审批数据
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetApprovalActivity")]
+        public List<ApprovalActivity> GetApprovalActivity()
+        {
+            var approvalActivityList = ApprovalActivity.GetApprovalActivity();
+            return approvalActivityList;
+        }
+    }
+}
---
+++ SunFlower/SunFlower.Api/Controllers/ApprovalProcessController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Data;
+using SunFlower.MODEL;
+using SunFlower.Services;
+using Unity.Attributes;
+using SunFlower.IServices;
+
+namespace SunFlower.Api.Controllers
+{
+    [RoutePrefix("ApprovalProcess")]
+    public class ApprovalProcessController : ApiController
+    {
+        [Dependency]
+        public IApprovalProcess ApprovalProcess { get; set; }
+
+        /// <summary>
+        /// 显示审批
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetApprovalProcess")]
+        public List<ApprovalProcess> GetApprovalProcess()
+        {
+            var approvalProcessList = ApprovalProcess.GetApprovalProcess();
+            return approvalProcessList;
+        }
+        /// <summary>
+        /// 删除审批
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("DeleteApprovalProcess")]
+        public int DeleteApprovalProcess(int id)
+        {
+            var result = ApprovalProcess.DeleteApprovalProcess(id);
+            return result;
+        }
+
+        /// <summary>
+        /// 添加流程
+        /// </summary>
+        /// <param name="approvalProcess"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("AddApprovalProcess")]
+        public int AddApprovalProcess(ApprovalProcess approvalProcess)
+        {
+            var result = ApprovalProcess.AddApprovalProcess(approvalProcess);
+            return result;
+        }
+
+        /// <summary>
+        /// 修改流程
+        /// </summary>
+        /// <param name="approvalProcess"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("UpdateApprovalProcess")]
+        public int UpdateApprovalProcess(Food approvalProcess)
+        {
+            var result = ApprovalProcess.UpdateApprovalProcess(approvalProcess);
+            return result;
+        }
+
+        /// <summary>
+        /// 获取单个审批
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetApprovalProcessByID")]
+        public List<ApprovalProcess> GetApprovalProcessByID(int id)
+        {
+            var result = ApprovalProcess.GetApprovalProcessByID(id);
+            return result;
+        }
+    }
+}
