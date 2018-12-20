574fd9cb860e99f4a1d4fea1200a620980d66eef
liuyikai152 <liuyknet.163.com>
2018-12-20 18:44:52 +0800

    ytq

---
+++ SunFlower/SunFlower.IServices/IApprovalActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunFlower.IServices
+{
+    using MODEL;
+
+    public interface IApprovalActivity
+    {
+        /// <summary>
+        /// 添加审批活动
+        /// </summary>
+        /// <returns></returns>
+        int AddApprovalActivity();
+
+        /// <summary>
+        /// 显示审批活动
+        /// </summary>
+        /// <returns></returns>
+        List<ApprovalActivity> GetApprovalActivity();
+    }
+}
